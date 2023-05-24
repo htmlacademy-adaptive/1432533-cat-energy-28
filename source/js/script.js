614427335581ced998f9975736d6f8f87124c2c3
Михаил Емцев <vrnvoid@gmail.com>
2023-05-24 23:41:49 +0300

    Задание 6

--- source/js/script.js
+++ source/js/script.js
@@ -2,13 +2,9 @@ let navMain = document.querySelector(`.main-nav`)
 let navToggle = document.querySelector(`.main-nav__toggle`)
 
 navMain.classList.remove(`main-nav--nojs`)
+navMain.classList.replace(`main-nav--opened`, `main-nav--closed`)
 
 navToggle.addEventListener(`click`, () => {
-	if (navMain.classList.contains(`main-nav--closed`)) {
-		navMain.classList.remove(`main-nav--closed`)
-		navMain.classList.add(`main-nav--opened`)
-	} else {
-		navMain.classList.add(`main-nav--closed`)
-		navMain.classList.remove(`main-nav--opened`)
-	}
+  navMain.classList.toggle(`main-nav--closed`)
+  navMain.classList.toggle(`main-nav--opened`)
 })
